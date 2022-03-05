5ec655a315954dee0274a5020c874a61e8aba18d
Sergey <serj_bibox@mail.ru>
2022-03-05 16:20:04 +0600

    master

--- daos/dbsettings.go
+++ daos/dbsettings.go
@@ -32,13 +32,6 @@ func ConnStringConfig() (string, error) {
 		FSTR_DB_HOST:  "",
 		FSTR_DB_PORT:  "",
 	}
-	// Установка системных переменных для тестирования
-	// После теста удалить!
-	os.Setenv(FSTR_DB_LOGIN, "fstr")
-	os.Setenv(FSTR_DB_PASS, "123456")
-	os.Setenv(FSTR_DB_HOST, "35.239.250.100")
-	os.Setenv(FSTR_DB_PORT, "5432")
-	//////////////////////////////////////////////////
 	for key := range m {
 		if d := readEnvironment(key); d == "" {
 			return "", errors.New("Системная переменная не найдена: " + key)
