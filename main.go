ee987f9cac5646c915233e8585fa3ecd87d5fd00
serjbibox <46563917+serjbibox@users.noreply.github.com>
2022-03-11 00:45:51 +0600

    Update main.go

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func main() {
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.URLFormat)
 	r.Use(render.SetContentType(render.ContentTypeJSON))
-	r.Get("/*", httpSwagger.Handler(httpSwagger.URL("http://fstr-app.herokuapp.com//swagger/doc.json")))
+	r.Get("/*", httpSwagger.Handler(httpSwagger.URL("http://fstr-app.herokuapp.com/swagger/doc.json")))
 	
 	// API definition localhost
 	//r.Get("/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8080/swagger/doc.json")))
