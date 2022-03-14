c4d3f7338c2855258a19226ef7f03f1c7a5ec7e5
Sergey <serj_bibox@mail.ru>
2022-03-14 11:03:03 +0600

    master

--- daos/dbsettings.go
+++ daos/dbsettings.go
@@ -2,8 +2,6 @@ package daos
 
 import (
 	"database/sql"
-	"errors"
-	"log"
 	"os"
 )
 
@@ -27,34 +25,36 @@ var DB *sql.DB
 var DbConnString string
 
 func ConnStringConfig() (string, error) {
-	m := map[string]string{
-		FSTR_DB_LOGIN: "",
-		FSTR_DB_PASS:  "",
-		FSTR_DB_HOST:  "",
-		FSTR_DB_PORT:  "",
-	}
-	for key := range m {
-		if d := readEnvironment(key); d == "" {
-			return "", errors.New("Системная переменная не найдена: " + key)
-		} else {
-			m[key] = d
+	return "postgres://ppbwkxkrjzbksv:ed30e87a9d372447e4910d34b84c653ac6f67209f6a4034390f791a810f6c8ca@ec2-52-70-186-184.compute-1.amazonaws.com:5432/dci84nq5jb8beh", nil
+	/*
+		m := map[string]string{
+			FSTR_DB_LOGIN: "",
+			FSTR_DB_PASS:  "",
+			FSTR_DB_HOST:  "",
+			FSTR_DB_PORT:  "",
 		}
-	}
-	connString := "postgres://" +
-		m[FSTR_DB_LOGIN] +
-		":" +
-		m[FSTR_DB_PASS] +
-		"@" +
-		m[FSTR_DB_HOST] +
-		":" +
-		m[FSTR_DB_PORT] +
-		"/" +
-		FSTR_DB_NAME // + "?sslmode=disable"
+		for key := range m {
+			if d := readEnvironment(key); d == "" {
+				return "", errors.New("Системная переменная не найдена: " + key)
+			} else {
+				m[key] = d
+			}
+		}
+		connString := "postgres://" +
+			m[FSTR_DB_LOGIN] +
+			":" +
+			m[FSTR_DB_PASS] +
+			"@" +
+			m[FSTR_DB_HOST] +
+			":" +
+			m[FSTR_DB_PORT] +
+			"/" +
+			FSTR_DB_NAME // + "?sslmode=disable"
 
-	log.Println(connString)
+		log.Println(connString)
 
-	return connString, nil
-
+		return connString, nil
+	*/
 }
 
 func readEnvironment(key string) string {
